96ef50caca69ce96b38d4b6a389294083b3854d5
Aeired <158105153+Aeired@users.noreply.github.com>
2024-04-29 15:45:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  describe('a rotating spinning top');
+}
+
+function draw() {
+  
+  background(200);
+  fill(165, 132, 87);
+  
+  rotateX(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  
+  cone(50,70);
+  translate(0,-30)
+  cylinder(5,80);
+  
+}
